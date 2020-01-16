49834c7e2af72486156d6ba35acbbde07f32cc8c
BispoJs <jsbispo@outlook.com>
2020-01-16 14:35:17 -0300

    init front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 function App() {
   return (
-   <h1>Hello World</h1>
+    // div externar, par organizar todo o layout
+    <div id="app">
+      {/* tag html usada para criar side-bar */}
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username" >Usuário do GitHub</label>
+            <input name="github_username" id="github_username"></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs" >Técnologias</label>
+            <input name="techs" id="techs"></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude" >Latitute</label>
+              <input name="latitude" id="latitude"></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude" >Longitude</label>
+              <input name="longitude" id="longitude"></input>
+            </div>
+          </div>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </aside >
+      {/* tag usada para organizar conteudo principal da pag */}
+      < main >
+
+      </main >
+    </div >
   );
 }
 
